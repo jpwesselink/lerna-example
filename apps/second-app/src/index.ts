f2035457f7b0ea6fc604bcb514f1f06819e1b236
JP Wesselink <jpwesselink@gmail.com>
2022-11-24 11:20:50 +0100

    trigger deployment for 2nd app

--- apps/second-app/src/index.ts
+++ apps/second-app/src/index.ts
@@ -1,4 +1,4 @@
 import common from "@packages/common";
 import comeAgain from "@packages/come-again";
 
-console.log("Hello from second-app", common(), comeAgain());
+console.log("Hello from second-app!!1222", common(), comeAgain());
